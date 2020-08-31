46d1ead1602b7d91e1997abdfa868673bb52b734
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-08-31 08:04:07 +0200

    use translator from 3d_armor
    related: https://github.com/minetest-mods/3d_armor/pull/12

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
 -- support for i18n
-local S = armor_i18n.gettext
-local F = armor_i18n.fgettext
+local S = minetest.get_translator("3d_armor")
 
 if not minetest.get_modpath("technic_worldgen") then
 	minetest.log("warning", S("technic_armor: Mod loaded but unused."))
